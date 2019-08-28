b6818d1775ae90a99fbfd92e665a8b458181df71
wyeth0725 <c125054ycomwork.c.oka-pu.ac.jp>
2019-08-29 00:21:19 +0900

    first commit

---
+++ test_suites.py
@@ -0,0 +1,45 @@
+import unittest
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from builtins import classmethod 
+
+class HomePageTest(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.driver = webdriver.Chrome(r"C:\Users\koga\Documents\chromedriver_win32\chromedriver.exe")
+        cls.driver.implicitly_wait(30)
+        cls.driver.get("https://www.monotaro.com")
+
+    def test_search_field(self):
+        self.assertTrue(self.is_element_present(By.NAME, "q"))
+    
+    def test_language_option(self):
+        self.assertTrue(self.is_element_present(By.ID, "new_nav"))
+
+    def test_shopping_cart_empty_message(self):
+        shopping_cart_icon = self.driver.find_element_by_css_selector("a.globalMenu__menu--basket basket_idle")
+        shopping_cart_icon.click()
+        
+        shopping_cart_status = self.driver.find_element_by_css_selector("p.basket__iteNone__message").text
+        self.assertEqual("現在バスケットには何も入っていません。", shopping_cart_status)
+        
+        close_button = self.driver.find_element_by_css_selector("a.basket__iteNone__link")
+        close_button.click()
+    
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by = how, value = what)
+        except NoSuchElementException as e:
+            return False
+        return True 
+    
+@classmethod
+def tear_down(cls):
+    cls.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main(verbosity = 2)
+
+
+    
